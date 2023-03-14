9f34fe0b93a37cd74d27fc056a71b3ad9f0eff03
masanori.tsuruta <masanori.tsuruta@zenken.co.jp>
2023-03-14 09:59:37 +0900

    途中

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/chinzansou',
-    outDir: './dist/chinzansou',
+  base: '/chinzansou',
+  outDir: './dist/chinzansou',
+  integrations: [image()]
 });
