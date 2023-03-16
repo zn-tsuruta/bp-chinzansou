fb3ba98ebd7360131ae4dc509f0e001e967c8131
鶴田 政則 <masanori.tsuruta@zenken.co.jp>
2023-03-16 21:29:49 +0900

    ipによる動機設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import image from "@astrojs/image";
 export default defineConfig({
   base: '/chinzansou',
   outDir: './dist/chinzansou',
-  integrations: [image()]
+  integrations: [image()],
+  //server:{ port:88, host: '192.168.193.87' }, // wifi
+  server:{ port:88, host: '192.168.22.87' } // 有線
 });
