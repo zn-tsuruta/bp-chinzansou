b905ea29a0a1b1f986c4bf3749b3bf0160f473ad
鶴田 政則 <masanori.tsuruta@zenken.co.jp>
2023-04-10 11:15:23 +0900

    まとめてプッシュ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/chinzansou',
-  outDir: './dist/chinzansou',
-  integrations: [image()],
-  //server:{ port:88, host: '192.168.193.87' }, // wifi
+  //base: '/chinzansou',
+  outDir: './dist',
+  integrations: [image(),relativeLinks()],
+ // server:{ port:88, host: '192.168.193.87' }, // wifi
   server:{ port:88, host: '192.168.22.87' } // 有線
 });
